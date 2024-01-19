9ff211c838d7c76a58fe1672b51cc9d2bb5fe353
mbouanik <mbouanik@student.42.fr>
2024-01-19 08:46:46 -0600

    routes added

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-  return res.send("");
+app.get("/mean", (req, res, next) => {
+  return res.send("mean");
+});
+
+app.get("/median", (req, res, next) => {
+  return res.send("median");
+});
+
+app.get("/mode", (req, res, next) => {
+  return res.send("mode");
+});
+
+app.listen(3000, () => {
+  console.log("app started at port:3000");
 });
